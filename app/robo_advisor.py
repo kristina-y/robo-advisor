ef0fb9a0ac624f8260d74d433aae2688f4b79e4b
Kristina Yarovinsky <ky226@georgetown.edu>
2021-03-07 12:08:36 -0500

    Get latest high price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -29,9 +29,18 @@ dates = list(tsd.keys()) # TO DO: assumes first day is on top,
 latest_day = dates[0]
 
 latest_close = tsd[latest_day]["4. close"]
+
+# get the high price from each day
+high_prices = []
+
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    high_prices.append(float(high_price))
+#maximum of all the high prices
+recent_high = max(high_prices)
+
 # Info outputs
 
-# this is the "app/robo_advisor.py" file
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
@@ -41,7 +50,7 @@ print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
